f821df48b4c05bd87ce816fd7468b01d1387a22e
Bruno Florentino <brunoribeirofox@gmail.com>
2023-07-21 01:12:41 -0300

    validacao de struct opening, adaptacao logger,

--- handler/handler.go
+++ handler/handler.go
@@ -1,12 +1,17 @@
 package handler
 
+import (
+	"api-opportunities/config"
 
+	"gorm.io/gorm"
+)
 
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
 
-
-
-
-
-
-
-
+func InitializeHandler(){
+	logger = config.GetLogger("Handler")
+	db = config.GetSQLite()
+}
